8f801de64ae03f8d8e3176ecfd98ef6c43355672
jacob-work <jacobbishopxy@gmail.com>
2023-03-06 15:52:28 +0800

    MNT: 修改 py 例子

---
+++ dev.py
@@ -0,0 +1,9 @@
+# @file:	dev.py
+# @author:	Jacob Xie
+# @date:	2023/03/06 15:21:09 Monday
+# @brief:	test
+
+from qi.common_tools.calendar_utils import ashare_cal
+
+ashare_cal.get_trading_day("20230306", 0)
+
